d04133f96d02479e399e200a6237529d1afff8d3
Tyler Long <tylerlong58@outlook.com>
2022-01-22 12:23:13 -0600

    Added connectivity to Google Sheets

---
+++ src/views/SheetTest.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<main role="main" class="col-md-12 ml-sm-auto col-lg-12 pt-3 px-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 ">
+					<h2>VueSheet List</h2>
+					<div class="btn-toolbar mb-2 mb-md-0">
+						<a 	href="https://docs.google.com/spreadsheets/d/1Dv6ezkTo6wO4pB_-NxcscUdmc-RPWAREtZQYNK0GZNg/edit?usp=sharing" 
+							class="btn btn-sm btn-outline-secondary"
+							target="_blank">
+						View Google Sheet
+						</a>
+					</div>
+				</div>
+				<div class="table-responsive">
+				<table class="table table-striped ">
+					<thead>
+					<tr>
+						<th>Name</th>
+						<th>Phone</th>
+						<th>Activity</th>
+						<th>City</th>
+					</tr>
+					</thead>
+					
+					<tbody>
+						<Row v-bind:key="row.id" v-for="row in rows" v-bind:row="row" />
+					</tbody>
+				</table>
+				</div>
+			</main>
+		</div>
+	</div>
+</template>
+ 
+<script>
+import Row from '@/components/Row.vue';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require('@/client_secret.json');
+	export default {
+		name: "Sheet",
+		components: {
+			Row
+		},
+		props: ["sheet"],
+		data() {
+			return {
+				rows: [],
+				loading: true,
+			}
+		},
+		methods:{
+			async accessSpreadSheet() {
+				const doc = new GoogleSpreadsheet('1hFVSy7FTLIbweePF_yKqB1YbfvxtfNxl8_YW957dFrU');
+				await doc.useServiceAccountAuth(creds);
+				await doc.loadInfo(); 
+				const sheet = doc.sheetsByIndex[0];
+				const  rows = await sheet.getRows({
+					offset: 0
+				})
+				this.rows = rows;
+			}
+		},
+		created() {
+			this.accessSpreadSheet();
+		}
+		
+	}
+</script>
+
+<style scoped>
+</style>
