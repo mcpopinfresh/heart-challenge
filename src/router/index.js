ce86a2be07cf442df4542d2e6a6a1393b593bbd4
Tyler Long <tylerlong58@outlook.com>
2022-01-22 12:29:15 -0600

    Actually add stuff for Google sheets

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SheetTest from '../views/SheetTest.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/sheetTest',
+    name: 'SheetTest',
+    component: SheetTest
   },
   {
     path: '/about',
